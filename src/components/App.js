bc082e5d0b11af9da75f8e8d9c4b3dc68164961b
milesros <miles.rostami@gmail.com>
2018-07-21 18:53:36 -0400

    added ability to sort

--- src/components/App.js
+++ src/components/App.js
@@ -10,16 +10,37 @@ class App extends Component {
   state = {
     search: '',
     count: 100,
+    order: 'popular',
     images: []
   };
+
+  onOrderChange = order => {
+    this.setState({ order }, () => {
+      if (this.state.search.length !== 0) {
+        const { search, count, order } = this.state;
+        let url = `https://pixabay.com/api/?key=${
+          keys.apiKey
+        }&q=${search}&image_type=photo&order=${order}&per_page=${count}&safesearch=true`;
+        fetch(url)
+          .then(data => data.json())
+          .then(response => {
+            this.setState({
+              images: response.hits
+            });
+          })
+          .catch(err => console.log(err));
+      }
+    });
+  }
+  
 
   OnChangeInput = debounce(search => {
     this.setState({ search }, () => {
       if (this.state.search.length !== 0) {
-        const { search, count } = this.state;
+        const { search, count, order } = this.state;
         let url = `https://pixabay.com/api/?key=${
           keys.apiKey
-        }&q=${search}&image_type=photo&per_page=${count}&safesearch=true`;
+        }&q=${search}&image_type=photo&order=${order}&per_page=${count}&safesearch=true`;
         fetch(url)
           .then(data => data.json())
           .then(response => {
@@ -43,6 +64,23 @@ class App extends Component {
             onChange={e => this.OnChangeInput(e.target.value)}
           />
           <label className="search-label">Search</label>
+          <div className="sort">
+            Sort by:
+            <a
+              className={this.state.order === 'popular' ? 'sort-active' : null}
+              onClick={() => this.onOrderChange('popular')}
+              href="#"
+            >
+              Popular
+            </a>|
+            <a
+              className={this.state.order === 'latest' ? 'sort-active' : null}
+              onClick={() => this.onOrderChange('latest')}
+              href="#"
+            >
+              Latest
+            </a>
+          </div>
         </div>
         {this.state.images.length > 0 ? (
           <ImageGrid images={this.state.images} />
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
   return (
-    <div class="made-with-love">
+    <div className="made-with-love">
       Made with <i>♥</i> by{' '}
       <a target="_blank" href="https://milesr.com" rel="noopener noreferrer">
         Miles Rostami
